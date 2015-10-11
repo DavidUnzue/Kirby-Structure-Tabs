dd60f6d839f154c913ece070da78deb9cd1ffe6d
David Unzue <davidunzue@gmail.com>
2015-10-11 14:14:39 +0200

    Fixed issue with tabs outside of the structure modal

--- assets/js/structuretab.js
+++ assets/js/structuretab.js
@@ -2,57 +2,61 @@
   $(document).ready(function() {
 
     if (app && typeof app == 'object') {
-      $(app.modal).on('view:load', initModalModules);
+      $(app.modal).on('view:load', function() {
+        // add this class to avoid issues with tabs outside from structure modal
+        $(this).addClass('structuretab-wrapper');
+        initModalModules();
+      });
     }
 
     function initModalModules() {
-      
-    if ($('.modal-content input.structuretab').length)
 
-      if ($(".modal-content").length){
-          $('.modal-content').removeClass("modal-content-fixed");
-          $(".modal-content .form").prepend($("<ul></ul>").addClass("tabs"));
+    if ($('.structuretab-wrapper .modal-content input.structuretab').length)
+
+      if ($(".structuretab-wrapper .modal-content").length){
+          $('.structuretab-wrapper .modal-content').removeClass("modal-content-fixed");
+          $(".structuretab-wrapper .modal-content .form").prepend($("<ul></ul>").addClass("tabs"));
       }
 
       else{
-        $('.modal-content .input.structuretab').first().closest('.field-grid-item').prepend($("<ul></ul>").addClass("tabs"));
+        $('.structuretab-wrapper .modal-content .input.structuretab').first().closest('.field-grid-item').prepend($("<ul></ul>").addClass("tabs"));
       }
 
-      $('.modal-content label.structuretab').each(function() {
+      $('.structuretab-wrapper .modal-content label.structuretab').each(function() {
         var title = "<li class='tab' href='" + $(this).attr('name') + "'>" + $(this).closest(".field-grid-item").text().trim() + "</li>";
-        $(".tabs").append(title);
-      });
-      
-      $('.modal-content label.structuretab').each(function() {
-        $(this).closest(".field-grid-item").addClass("group-title");
-      });
-      
-      $('.modal-content .group-title').each(function() {
-        $(this).nextUntil(".modal-content .group-title").andSelf().wrapAll('<div class="group" />');
+        $(".structuretab-wrapper .tabs").append(title);
       });
 
-      $('.modal-content .group').each(function() {
+      $('.structuretab-wrapper .modal-content label.structuretab').each(function() {
+        $(this).closest(".field-grid-item").addClass("group-title");
+      });
+
+      $('.structuretab-wrapper .modal-content .group-title').each(function() {
+        $(this).nextUntil(".structuretab-wrapper .modal-content .group-title").andSelf().wrapAll('<div class="group" />');
+      });
+
+      $('.structuretab-wrapper .modal-content .group').each(function() {
         var id = $(this).find(".group-title label.structuretab").attr('name');
         $(this).attr('id', id);
       });
 
-      if (!$('.modal-content .group.current').length) {
-        $('.tabs li').first().addClass('current');
-        var first_group = $('.group').first();
+      if (!$('.structuretab-wrapper .modal-content .group.current').length) {
+        $('.structuretab-wrapper .tabs li').first().addClass('current');
+        var first_group = $('.structuretab-wrapper .group').first();
         first_group.addClass('current');
         first_group.find('input.structuretab').attr('checked', true);
       }
 
-      $('.modal-content .tabs li').click(function() {
+      $('.structuretab-wrapper .modal-content .tabs li').click(function() {
         $(this).addClass("current");
         $(this).siblings().removeClass("current");
         var tab_id = $(this).attr('href');
-        $('.group').removeClass('current');
-        $("#" + tab_id).addClass('current');
-        $("#" + tab_id).find('input.structuretab').prop('checked', true);
-        $("#" + tab_id).siblings().find('input.structuretab').prop('checked', false);
+        $('.structuretab-wrapper .group').removeClass('current');
+        $(".structuretab-wrapper #" + tab_id).addClass('current');
+        $(".structuretab-wrapper #" + tab_id).find('input.structuretab').prop('checked', true);
+        $(".structuretab-wrapper #" + tab_id).siblings().find('input.structuretab').prop('checked', false);
       });
-    
+
     }
 
 
